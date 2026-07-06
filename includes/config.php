276297977f714b1bc60bd22412aec69e1f16117b
jblustre0125 <jblustre0025@gmail.com>
2026-07-06 09:21:02 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -66,7 +66,7 @@ define('PICK_TAG_WIDTH_HUNDREDTHS', 300);  // 3.00 inches
 define('PICK_TAG_HEIGHT_HUNDREDTHS', 300); // 3.00 inches
 define('PICK_TAG_IMAGE_SCALE', 1);         // 1 = 609px/203 DPI, 2 = 1218px/high-res
 define('PICK_TAG_IMAGE_PNG_COMPRESSION', 6);
-define('PICK_TAG_DRIVER_RELAY_ENABLED', false);
+define('PICK_TAG_DRIVER_RELAY_ENABLED', true);
 define('PICK_TAG_DRIVER_RELAY_INBOX', '\\\\192.168.23.64\\NittoPrintRelay\\inbox');
 define('PICK_TAG_DRIVER_RELAY_PRINTER', 'NITTO DURA-SL-400');
 
