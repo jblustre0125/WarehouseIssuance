ada79786e5bb4baaad72103b9ec10bd95969ea97
jblustre0125 <jblustre0025@gmail.com>
2026-08-11 11:36:47 +0800

    fix the grpo cache

--- tools/run_picker_grpo_cache.cmd
+++ tools/run_picker_grpo_cache.cmd
@@ -1,3 +1,17 @@
 @echo off
-if not exist "C:\Xampp\htdocs\WarehouseIssuance\storage\logs" mkdir "C:\Xampp\htdocs\WarehouseIssuance\storage\logs"
-"C:\Xampp\php\php.exe" "C:\Xampp\htdocs\WarehouseIssuance\tools\sync_picker_grpo_cache.php" 30 >> "C:\Xampp\htdocs\WarehouseIssuance\storage\logs\picker_grpo_cache_sync.log" 2>&1
+set "APP_DIR=C:\Xampp\htdocs\WarehouseIssuance"
+set "PHP_EXE=C:\Xampp\php\php.exe"
+set "SCRIPT=%APP_DIR%\tools\sync_picker_grpo_cache.php"
+set "LOG_DIR=%APP_DIR%\storage\logs"
+set "LOG_FILE=%LOG_DIR%\picker_grpo_cache_sync.log"
+
+if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
+
+>> "%LOG_FILE%" echo [%date% %time%] Task wrapper starting.
+
+cd /d "%APP_DIR%"
+"%PHP_EXE%" "%SCRIPT%" 7 >> "%LOG_FILE%" 2>&1
+set "EXIT_CODE=%ERRORLEVEL%"
+
+>> "%LOG_FILE%" echo [%date% %time%] Task wrapper finished with exit code %EXIT_CODE%.
+exit /b %EXIT_CODE%
